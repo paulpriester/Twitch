c19613f75d4370c2b2d19378ef1d6437a7eeba0b
Paul Priester <paulpriester507@gmail.com>
2019-06-13 02:04:44 -0400

    BoilerPlate Project

--- client/src/component/streams/StreamDelete.js
+++ client/src/component/streams/StreamDelete.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Modal from '../Modal';
-import createBrowserHistory from '../../history' 
-const StreamDelete = () => {
-	const actions = (
+import createBrowserHistory from '../../history'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchStream, deleteStream } from '../../actions'
+
+class StreamDelete extends Component {
+	componentDidMount () {
+		this.props.fetchStream(this.props.match.params.id)
+	}
+
+	renderActions() {
+		const id = this.props.match.params.id
+		return (
 		<React.Fragment>
-			<button className='ui button negative'>Delete</button>
-			<button className='ui button'>Cancel</button>
+			<button onClick={() => this.props.deleteStream(id)} className='ui button negative'>Delete</button>
+			<Link to='/' className='ui button'>Cancel</Link>
 		</React.Fragment>
 		)
-	return (
-		<div>
-			StreamDelete
+	}
+
+	renderContent() {
+		if (!this.props.stream) {
+			return 'Are you sure you want to delete this stream?'
+		}
+		return `Are you sure you want to delete this stream with title: ${this.props.stream.title}?`
+	}
+
+	render () {
+		return (
 			<Modal 
 				title='Delete Stream'
-				content='Are you sure you want to delete this stream?'
-				actions={actions}
+				content= {this.renderContent()}
+				actions={this.renderActions()}
 				onDismiss={() => createBrowserHistory.push('/')}
-			/>
-		</div>
-	)
+				/>
+		)
+	}
 }
 
-export default StreamDelete
+	const mapStateToProps = (state,ownProps) => {
+		return {stream: state.streams[ownProps.match.params.id]}
+	}
+
+export default connect(mapStateToProps, { fetchStream, deleteStream})(StreamDelete)
--- client/src/component/streams/StreamList.js
+++ client/src/component/streams/StreamList.js
@@ -15,9 +15,9 @@ class StreamList extends Component {
 					<Link to={`/streams/edit/${stream.id}`} className='ui button primary'>
 						Edit
 					</Link>
-					<button className='ui button negative'>
+					<Link to={`/streams/delete/${stream.id}`} className='ui button negative'>
 						Delete
-					</button>
+					</Link>
 				</div>
 			)
 		}
@@ -30,7 +30,9 @@ class StreamList extends Component {
 					{this.renderAdmin(stream)}
 					<i className= 'large middle aligned icon camera' />
 					<div className= 'content'>
+						<Link className='header' to={`/streams/${stream.id}`}>
 						{stream.title}
+						</Link>
 						<div className= 'description'>
 							{stream.description}
 						</div>
